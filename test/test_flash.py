444bb4272951a86cb75127c833f089f25d467cc3
Alex Degtiar <alex.degtiar@gmail.com>
2013-11-09 20:07:02 -0500

    Adding test module and stub flash test

---
+++ test/test_flash.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Tests the flash-writing backend library."""
+
+import unittest
+
+from fawnlog import flashlib
+
+
+class TestFlash(unittest.TestCase):
+    """Tests the basic flash backend functionality."""
+
+    def test_stub(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
