59bd63f42c63a2e1d7c9aed8ec73020334e33402
Alex Degtiar <alex.degtiar@gmail.com>
2013-11-09 20:57:11 -0500

    Adding interface for flashlib

--- fawnlog/flashlib.py
+++ fawnlog/flashlib.py
@@ -4,3 +4,22 @@ Implements the write-once page-based flash interface as a file-backed
 data store.
 
 """
+
+class FlashStore(object):
+    """A page-based flash storage interface backed by a file.
+
+    Supports writing to an infinite address range of pages. Writes must
+    fit within a page. Enforces write-once semantics.
+
+    """
+
+    def __init__(self, file_path):
+        pass
+
+    def write_page(self, data, offset):
+        """Writes the data to a page at the given offset."""
+        pass
+
+    def read_page(self, offset):
+        """Reads the data from the page at the given offset."""
+        pass
