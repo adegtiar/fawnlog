2ca641d244be64e106315242658007dfbd532f57
Veronica <zhengzhixing@gmail.com>
2013-11-10 19:02:19 -0500

    Fix the flash client example

--- fawnlog/flash_client_tmp.py
+++ fawnlog/flash_client_tmp.py
@@ -8,32 +8,32 @@ import flash_service_pb2
 from protobuf.socketrpc import RpcService
 
 
-(host, port) = config.SERVER_ADDR_LIST[0]
+(host, port) = config.SERVER_ADDR_LIST[5]
 # Create a new service instance
 service = RpcService(flash_service_pb2.FlashService_Stub,
-                     host, port)
+                     port, host)
 
-#  Read
-request_r = flash_service_pb2.ReadRequest()
-request_r.offset = 10000
-
-# Make a synchronous call
-try:
-    print "Call read"
-    response_r = service.Read(request_r, timeout=10000)
-    print response_r.status
-    print response_r.data
-except Exception, ex:
-    print ex
-
+offset = 10000
 
 # Write
 request_w = flash_service_pb2.WriteRequest()
-request_w.offset = 1123123
+request_w.offset = offset
 request_w.data = "datadatadatadatadatadata"
 try:
     print "Call write"
     response_w = service.Write(request_w, timeout=10000)
     print response_w.status
+except Exception, ex:
+    print ex
+
+#  Read
+request_r = flash_service_pb2.ReadRequest()
+request_r.offset = offset
+try:
+    print "Call read"
+    response_r = service.Read(request_r, timeout=10000)
+    print response_r.status
+    if response_r.status == flash_service_pb2.ReadResponse.SUCCESS:
+        print response_r.data
 except Exception, ex:
     print ex
