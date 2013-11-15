2abacac3382181174f92f92a2ce1c562adea9c19
circulardark <circulardark@gmail.com>
2013-11-15 10:50:37 -0500

    add test_client

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -7,5 +7,6 @@ python -m test.test_get_token_service
 echo "Running flash service tests"
 python -m test.test_flash_service
 python -m test.test_projection
+python -m test.test_client
 rm fawnlog/*.pyc
 rm test/*.pyc
