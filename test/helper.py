d9bc4f64e346ce103541eda8346487dd53f9a969
Veronica <zhengzhixing@gmail.com>
2013-11-15 22:18:00 -0500

    Fix ServerThread start_server bug

--- test/helper.py
+++ test/helper.py
@@ -1,6 +1,7 @@
 """Helper classes used for testing"""
 
 import protobuf.socketrpc.server
+import socket
 import threading
 import time
 
@@ -13,20 +14,25 @@ class ServerThread(threading.Thread):
         """
         threading.Thread.__init__(self)
 
+        self.port = port
+        self.host = host
         self.service = service
         self.server = protobuf.socketrpc.server.SocketRpcServer(port, host)
         self.server.registerService(self.service)
         self.setDaemon(True)
-        self.running = False
 
     def run(self):
-        self.running = True
         self.server.run()
 
     def start_server(self):
         self.start()
-        while not self.running:
-            time.sleep(1)
+        while True:
+            try:
+                socket.create_connection((self.host, self.port))
+            except socket.error:
+                continue
+            else:
+                return
 
     def reset(self, *args, **kwargs):
         self.service.reset(*args, **kwargs)
