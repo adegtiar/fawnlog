0d0f78a24e935fe4fed11e928e0c7d8a0ae379cb
Veronica <zhengzhixing@gmail.com>
2013-11-15 21:52:10 -0500

    Finish test_flash_service

--- test/test_flash_service.py
+++ test/test_flash_service.py
@@ -14,8 +14,7 @@ from fawnlog import flash_service
 
 FLASH_SERVER_PORT = 40001
 FLASH_SERVER_HOST = "127.0.0.1"
-PAGE_SIZE = 1024
-
+PAGE_SIZE = 4000
 
 class TestFlashService(unittest.TestCase):
     """Tests flash service functionality."""
@@ -41,31 +40,52 @@ class TestFlashService(unittest.TestCase):
         assert(reset_response.status == flash_service_pb2.ResetResponse.SUCCESS)
 
     def test_write_read_basic(self):
-        offset = 25
+        offset = 0
         data = os.urandom(PAGE_SIZE)
-        request_w = flash_service_pb2.WriteRequest()
-        request_w.offset = offset
-        request_w.data = data
-        response_w = TestFlashService.service.Write(request_w, timeout=10000)
+        response_w = self._write(offset, data)
         self.assertEqual(response_w.status,
             flash_service_pb2.WriteResponse.SUCCESS)
 
-        request_r = flash_service_pb2.ReadRequest()
-        request_r.offset = offset
-        response_r = TestFlashService.service.Read(request_r, timeout=10000)
+        response_r = self._read(offset)
         self.assertEqual(response_r.status,
             flash_service_pb2.ReadResponse.SUCCESS)
         self.assertEqual(response_r.data, data)
 
     def test_read_unwritten(self):
-        pass
+        offset = 5000
+        response_r = self._read(offset)
+        self.assertEqual(response_r.status,
+            flash_service_pb2.ReadResponse.ERROR_UNWRITTEN)
 
     def test_write_overwritten(self):
-        pass
+        offset = 10000
+        data = os.urandom(PAGE_SIZE)
+        # write once
+        self._write(offset, data)
+        # write again
+        response_w = self._write(offset, data)
+        self.assertEqual(response_w.status,
+            flash_service_pb2.WriteResponse.ERROR_OVERWRITTEN)
 
     def test_oversized_data(self):
-        pass
+        offset = 15000
+        data = os.urandom(PAGE_SIZE*2)
+        response_w = self._write(offset, data)
+        self.assertEqual(response_w.status,
+            flash_service_pb2.WriteResponse.ERROR_OVERSIZED_DATA)
+    
+    def _read(self, offset):
+        request_r = flash_service_pb2.ReadRequest()
+        request_r.offset = offset
+        response_r = TestFlashService.service.Read(request_r, timeout=10000)
+        return response_r
 
+    def _write(self, offset, data):
+        request_w = flash_service_pb2.WriteRequest()
+        request_w.offset = offset
+        request_w.data = data
+        response_w = TestFlashService.service.Write(request_w, timeout=10000)
+        return response_w
 
 if __name__ == "__main__":
     unittest.main()
