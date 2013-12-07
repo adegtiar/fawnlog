0d6948c838fc4ed1307df2ccbad06b609164112d
Alex Degtiar <alex.degtiar@gmail.com>
2013-12-06 23:22:06 -0500

    Making Sequencer thread-safe

--- fawnlog/sequencer.py
+++ fawnlog/sequencer.py
@@ -1,3 +1,5 @@
+import threading
+
 class Sequencer(object):
     """
     A simple sequencer which serialize requests and generates tokens.
@@ -5,15 +7,18 @@ class Sequencer(object):
 
     def __init__(self, start=0):
         self.counter = start
+        self.lock = threading.Lock()
 
     def reset(self, counter):
-        self.counter = counter
+        with self.lock:
+            self.counter = counter
 
     def get_token(self, number):
         """
         param: number: the number of flash pages wants to reserve.
         Return the current counter and increase it by number
         """
-        token = self.counter
-        self.counter += number
+        with self.lock:
+            token = self.counter
+            self.counter += number
         return token
