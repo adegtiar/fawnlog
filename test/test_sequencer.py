5ca6dcaf58be0379e8f2bd9c82dd7ee17629d55e
Veronica <zhengzhixing@gmail.com>
2013-12-09 17:11:49 -0500

    add a test in test_sequencer

--- test/test_sequencer.py
+++ test/test_sequencer.py
@@ -20,7 +20,7 @@ class TestSequencer(unittest.TestCase):
 
     def test_ideal_world(self):
         """The reqeusts come in order and always guess correctly,
-           no timeout should happen.
+        no timeout should happen.
         """
         sequencer = SequencerCore()
         try:
@@ -35,7 +35,7 @@ class TestSequencer(unittest.TestCase):
 
     def test_next_group(self):
         """The requests come in order and always guess correctly,
-           but will move to the next group.
+        but will move to the next group.
         """
         token = (config.FLASH_PER_GROUP - 1) * config.FLASH_PAGE_NUMBER
         sequencer = SequencerCore(token)
@@ -49,9 +49,9 @@ class TestSequencer(unittest.TestCase):
             sequencer.ips_thread.stop()
             sequencer.ips_thread.join()
 
-    def test_timeout(self):
+    def test_timeout_basic(self):
         """The requests guessed wrong and will timeout,
-           we will make a hole for it.
+        we will make a hole for it.
         """
         sequencer = SequencerCore()
         try:
@@ -71,11 +71,24 @@ class TestSequencer(unittest.TestCase):
             if sequencer.global_req_timer is not None:
                 sequencer.global_req_timer.cancel()
 
+    def test_adjust_cursor(self):
+        """There is a hole in the requests and after that all the requests
+        are continuous.
+        """
+        sequencer = SequencerCore()
+        start_index = config.FLASH_PER_GROUP - 1
+        try:
+            for i in xrange(100):
+                data_id = i + start_index
+                flash_unit_index = data_id % config.FLASH_PER_GROUP
+                sequencer.insert_request(str(data_id), flash_unit_index)
+                cursor = 0 if flash_unit_index > 0 else 1 
+                self.assertEqual(sequencer.cursor, cursor)
+        finally:
+           sequencer.ips_thread.stop()
+           sequencer.ips_thread.join()
+           if sequencer.global_req_timer is not None:
+               sequencer.global_req_timer.cancel()
+
 if __name__ == "__main__":
     unittest.main()
-
-
-        
-        
-        
-        
