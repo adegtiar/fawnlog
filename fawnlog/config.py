f95961f79b2da8426b89b81f2d89b561717b98f8
circulardark <circulardark@gmail.com>
2013-12-10 08:01:20 -0500

    config to 2

--- fawnlog/config.py
+++ fawnlog/config.py
@@ -1,16 +1,16 @@
-SEQUENCER_HOST = "54.200.180.127"
+SEQUENCER_HOST = "54.201.87.120"
 SEQUENCER_PORT = 10001
 
 COUNT_IPS_INTERVAL = 0.1 # seconds
 COUNT_IPS_ALPHA = 0.5 # weight for exponential moving average calculation
 REQUEST_TIMEOUT = 0.1 # seconds
 
-SERVER_ADDR_LIST = [("54.201.158.32", 10002),
-                    ("54.201.7.112", 10002),
-                    ("54.201.72.55", 10002),
-                    ("54.200.85.65", 10002),
-                    ("127.0.0.1", 10002),
-                    ("127.0.0.1", 10002),
+SERVER_ADDR_LIST = [("54.201.143.215", 10002),
+                    ("54.201.140.226", 10002),
+                    ("54.201.136.144", 10002),
+                    ("54.201.139.2", 10002),
+                    ("54.201.142.124", 10002),
+                    ("54.201.117.166", 10002),
                     ("127.0.0.1", 10002),
                     ("127.0.0.1", 10002)]
 
@@ -21,7 +21,7 @@ FLASH_HOLE_DELAY_THRESHOLD = 1.0
 FLASH_PAGE_NUMBER = 4000
 DEFAULT_BLOCK_SIZE = 4096
 FLASH_PAGE_SIZE = DEFAULT_BLOCK_SIZE - 4 # Includes header size.
-FLASH_PER_GROUP = 4 # number of servers per group
+FLASH_PER_GROUP = 2 # number of servers per group
 
 # Number of tokens ahead of approximation to guess.
 CLIENT_GUESS_OVERESTIMATION = 0
