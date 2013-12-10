cf61c410fb0782a35991ae0a11a30ef12e4b028c
circulardark <circulardark@gmail.com>
2013-12-09 21:56:21 -0500

    config changed

--- fawnlog/config.py
+++ fawnlog/config.py
@@ -1,18 +1,18 @@
-SEQUENCER_HOST = "127.0.0.1"
+SEQUENCER_HOST = "54.200.180.127"
 SEQUENCER_PORT = 10001
 
 COUNT_IPS_INTERVAL = 0.1 # seconds
 COUNT_IPS_ALPHA = 0.5 # weight for exponential moving average calculation
 REQUEST_TIMEOUT = 0.1 # seconds
 
-SERVER_ADDR_LIST = [("127.0.0.1", 10002),
-                    ("127.0.0.1", 10003),
-                    ("127.0.0.1", 10004),
-                    ("127.0.0.1", 10005),
-                    ("127.0.0.1", 10006),
-                    ("127.0.0.1", 10007),
-                    ("127.0.0.1", 10008),
-                    ("127.0.0.1", 10009)]
+SERVER_ADDR_LIST = [("54.201.158.32", 10002),
+                    ("54.201.7.112", 10002),
+                    ("54.201.72.55", 10002),
+                    ("127.0.0.1", 10002),
+                    ("127.0.0.1", 10002),
+                    ("127.0.0.1", 10002),
+                    ("127.0.0.1", 10002),
+                    ("127.0.0.1", 10002)]
 
 FLASH_FILE_PATH = "pagefile.flog"
 # The time interval (in seconds) since a flash unit receives an offset
