acf80922e3734f79c11950046d8d0a8c1734a7b4
circulardark <circulardark@gmail.com>
2013-12-09 19:48:25 -0500

    seq added to config

--- experiments/setup_flash.sh
+++ experiments/setup_flash.sh
@@ -1,17 +1,15 @@
 echo 'apt-get update'
 sudo apt-get update
 echo 'install pip'
-sudo apt-get install python-setuptools python-dev build-essential
+sudo apt-get install python-pip python-dev build-essential
 echo 'install git'
 sudo apt-get install git
 echo 'install protobuf'
 sudo pip install protobuf
 echo 'install bitarray'
 sudo pip install bitarray
-echo 'pull down codes'
-sudo git clone code
 echo 'git clone https://github.com/adegtiar/fawnlog'
-sudo git clone https://github.com/adegtiar/fawnlog
+git clone https://github.com/adegtiar/fawnlog
 echo 'install protobuf socketrpc'
 sudo easy_install fawnlog/protobuf.socketrpc-1.3.2-py2.7.egg
 echo 'start flash'
