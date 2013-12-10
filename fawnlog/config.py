b23eef036ad7ed6700192bfaa5b69653ed7c41bd
circulardark <circulardark@gmail.com>
2013-12-10 06:10:00 -0500

    first test

--- fawnlog/config.py
+++ fawnlog/config.py
@@ -1,16 +1,17 @@
-SEQUENCER_HOST = "54.200.180.127"
+SEQUENCER_HOST = "54.201.87.120"
 SEQUENCER_PORT = 10001
 
-SERVER_ADDR_LIST = [("54.201.158.32", 10002),
-                    ("54.201.7.112", 10002),
-                    ("54.201.72.55", 10002),
-                    ("54.200.85.65", 10002),
-                    ("127.0.0.1", 10002),
-                    ("127.0.0.1", 10002),
+SERVER_ADDR_LIST = [("54.201.143.215", 10002),
+                    ("54.201.140.226", 10002),
+                    ("54.201.136.144", 10002),
+                    ("54.201.139.2", 10002),
+                    ("54.201.142.124", 10002),
+                    ("54.201.117.166", 10002),
                     ("127.0.0.1", 10002)]
+
 
 FLASH_FILE_PATH = "pagefile.flog"
 FLASH_PAGE_NUMBER = 40000
 DEFAULT_BLOCK_SIZE = 2048
 FLASH_PAGE_SIZE = DEFAULT_BLOCK_SIZE - 4 # Includes header size.
-FLASH_PER_GROUP = 4 # number of servers per group
+FLASH_PER_GROUP = 2 # number of servers per group
