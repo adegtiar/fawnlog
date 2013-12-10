b1fbe3ceb144e798ff6ab8fd8bf0c9a8c64ccc78
circulardark <circulardark@gmail.com>
2013-12-09 21:08:07 -0500

    test_corfu modified

--- test_corfu.py
+++ test_corfu.py
@@ -12,25 +12,14 @@ import threading
 latencies = []
 
 
-def _start_sequencer():
-    sequencer_thread = helper.ServerThread(config.SEQUENCER_PORT,config.SEQUENCER_HOST, get_token_service.GetTokenImpl())
-    sequencer_thread.start_server()
-
-def _start_flash_servers():
-    server_index = 0
-    for (host, port) in config.SERVER_ADDR_LIST:
-        server_thread = helper.ServerThread(port, host,
-        flash_service.FlashServiceImpl(server_index))
-        server_index += 1
-        server_thread.start_server()
-
-def _client_impl(number_of_pages):
-    c = client.Client()
+def _client_impl(number_of_pages, client_number):
+    c = client.Client(config)
     latency_list = []
-    for _ in xrange(number_of_pages):
+    for i in xrange(number_of_pages):
         data_str_list = []
         for _ in xrange(config.FLASH_PAGE_SIZE):
             data_str_list.append(chr(random.randint(65, 90)))
+        print "client_{0} appends the {1}th page".format(client_number, i)
         data_str = ''.join(data_str_list)
         (_, ll) = c.append(data_str)
         latency_list.extend(ll)
@@ -38,8 +27,8 @@ def _client_impl(number_of_pages):
 
 def _start_clients(number_of_clients, number_of_pages):
     threads = []
-    for _ in xrange(number_of_clients):
-        threads.append(threading.Thread(target=_client_impl, args=(number_of_pages, )))
+    for i in xrange(number_of_clients):
+        threads.append(threading.Thread(target=_client_impl, args=(number_of_pages, i)))
     for i in xrange(number_of_clients):
         threads[i].start()
     for i in xrange(number_of_clients):
@@ -59,8 +48,6 @@ class TestCorfu(object):
         self.average_latency = 0.0
 
     def run(self):
-        _start_sequencer()
-        _start_flash_servers()
         _start_clients(number_of_clients, number_of_pages)
 
 
