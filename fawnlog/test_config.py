85cff1cdf85b721273389478a3aceaf0f3bc7e21
Veronica <zhengzhixing@gmail.com>
2013-12-09 20:17:05 -0500

    add test_config

---
+++ fawnlog/test_config.py
@@ -0,0 +1,16 @@
+SEQUENCER_HOST = "127.0.0.1"
+SEQUENCER_PORT = 10001
+
+SERVER_ADDR_LIST = [("127.0.0.1", 10002),
+                    ("127.0.0.1", 10003),
+                    ("127.0.0.1", 10004),
+                    ("127.0.0.1", 10005),
+                    ("127.0.0.1", 10006),
+                    ("127.0.0.1", 10007),
+                    ("127.0.0.1", 10008)]
+
+FLASH_FILE_PATH = "pagefile.flog"
+FLASH_PAGE_NUMBER = 40000
+DEFAULT_BLOCK_SIZE = 2048
+FLASH_PAGE_SIZE = DEFAULT_BLOCK_SIZE - 4 # Includes header size.
+FLASH_PER_GROUP = 3 # number of servers per group
